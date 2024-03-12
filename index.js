cd777a958ffc8a75122bb489925f8196c33d4ce1
Shu12388y <spl.sp999@gmail.com>
2024-03-13 01:03:34 +0530

    file added

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+
+client.initialize();
+
+client.on('message_create', message => {
+    // console.log(message.body);
+    if(message.body){
+
+        return message.reply('pong')
+    }
+});
+
