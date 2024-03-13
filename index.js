0be79c4fee418947d37e11d866d367e0a437bb40
Shu12388y <spl.sp999@gmail.com>
2024-03-13 21:28:48 +0530

    file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
+const Model = require("./model.js");
 
 const client = new Client({
   authStrategy: new LocalAuth({
@@ -15,10 +16,8 @@ client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-client.on("message_create", (message) => {
- 
-
-    // welcome message
+client.on("message_create", async (message) => {
+  // welcome message
   if (message.body.toLowerCase() === "/hello") {
     return message.reply(`"👋 Welcome to our WhatsApp Bot! 🤖
 
@@ -27,20 +26,21 @@ We're thrilled to have you here! 🎉 Our bot is here to assist you with any que
 To get started, simply type 'menu' for a list of available options, or type 'help' if you need assistance at any point.
         
 Feel free to explore and don't hesitate to reach out if you need any assistance. Happy chatting! 😊"`);
-}
+  }
 
-
-  
   //   ask questions
-if(message.body.toLowerCase().includes("/question")){
-    const q = message.body.slice(11,message.body.length)
-    
-}
-
-
-
-
-
+  if (message.body.toLowerCase().includes("/question")) {
+    const q = message.body.slice(10, message.body.length);
+    console.log(q);
+    const res = await Model(q);
+    console.log(res);
+    return message.reply(res.replace("Bot: ", ""));
+  } else {
+    return message.reply(`Sorry, I didn't understand that command. 🤔
+                          Please use one of the following commands:
+                          /hello - to greet me
+                          /question - to ask a question`);
+  }
 });
 
 client.initialize();
--- model.js
+++ model.js
@@ -2,6 +2,7 @@ const {
   BedrockRuntimeClient,
   InvokeModelCommand,
 } = require("@aws-sdk/client-bedrock-runtime");
+
 const config = {
   region: "us-east-1",
 };
@@ -10,25 +11,28 @@ const client = new BedrockRuntimeClient(config);
 
 async function Model(inputdata) {
   try {
-    console.log("Input Data:", inputdata);
     if (!inputdata) {
-        throw new Error("Input data is null or empty.");
+      throw new Error("Input data is null or empty.");
     }
     const input = {
-      body: inputdata,
+      body: `{\"inputText\":\"${inputdata}\",\"textGenerationConfig\":{\"maxTokenCount\":512,\"stopSequences\":[],\"temperature\":0,\"topP\":0.9}}`,
       contentType: "application/json",
       accept: "application/json",
       modelId: "amazon.titan-text-lite-v1",
     };
-    console.log("Command Input:", input);
     const command = new InvokeModelCommand(input);
     const response = await client.send(command);
-    console.log(response);
+    const stringData = JSON.parse(Buffer.from(response.body).toString());
+    const result = stringData.results[0].outputText
+
+    return result
+   
+
   } catch (error) {
     console.log("error", error);
   }
 }
 
-Model("hi");
 
-// export default Model
+
+module.exports = Model
